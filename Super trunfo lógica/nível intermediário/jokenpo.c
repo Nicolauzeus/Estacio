f6b69b7931b0cc447a39e5bfa47932715dde90c0
Nicolauzeus <capingananicolau@gmail.com>
2026-03-28 14:05:53 -0300

    jokenpo game add

---
+++ Super trunfo lógica/nível intermediário/jokenpo.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int jogador, computador;
+
+
+do {
+        printf("Jokenpo - Escolha sua jogada:\n");
+        printf("0 - Pedra\n");
+        printf("1 - Papel\n");
+        printf("2 - Tesoura\n");
+        printf("Digite sua jogada: ");
+        scanf("%d", &jogador);
+
+        if (jogador < 0 || jogador > 2) {
+            printf("Jogada inválida! Por favor, escolha entre 0, 1 ou 2.\n");
+        }
+    } while (jogador < 0 || jogador > 2); // Continua solicitando até que uma jogada válida seja feita 
+
+
+    srand(time(0)); // Inicializa a semente para geração de números aleatórios
+    computador = rand() % 3; // Gera um número aleatório entre 0 e 2
+
+
+    if (jogador < 0 || jogador > 2) {
+        printf("Jogada inválida! Por favor, escolha entre 0, 1 ou 2.\n");
+        return 1; // Encerra o programa com código de erro
+    }
+
+    printf("Computador escolheu: %d\n", computador);
+
+    if (jogador == computador) {
+        printf("Empate!\n");
+    } else if ((jogador == 0 && computador == 2) || (jogador == 1 && computador == 0) || (jogador == 2 && computador == 1))
+                {
+        printf("Você venceu!\n");
+    } else {
+        printf("Computador venceu!\n");
+    }
+
+    return 0;
+}
