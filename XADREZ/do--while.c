a794b0785ccde3b9ddb92a9d638010640722cd84
Nicolauzeus <capingananicolau@gmail.com>
2026-05-08 18:48:19 -0300

    do, do---while, for

---
+++ XADREZ/do--while.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int i = 500;
+
+    do {
+        printf("%d\n", i);
+        i++;
+    } while (i <= 1000);
+
+    return 0;
+}
---
+++ XADREZ/for.c
@@ -0,0 +1,14 @@
+/*for (inicialização; condição; incremento) {
+    // bloco de código a ser executado repetidamente
+}*/
+
+
+#include <stdio.h>
+ 
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
